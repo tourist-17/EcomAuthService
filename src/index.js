3b31b00b51e2d7c9fc6c52bf309ec80113d01db7
tourist-17 <yadsak00@gmail.com>
2024-02-24 21:04:46 +0530

    Added user and role model with complete authentication and authorisation

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const apiRoutes = require("./routes/index");
+const { PORT } = require("./config/serverConfig");
+const bodyParser = require("body-parser");
+const app = express();
+// const db = require("./models/index");
+// const {User,Role} = require("./models/index");
+
+
+// const UserService = require('./services/user-service');
+
+const prepareAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api", apiRoutes);
+  app.listen(PORT, async () => {
+    console.log(`Server Start on PORT ${PORT}`);
+    
+    // const u1 = await User.findByPk(3);
+    // const r1 = await Role.findByPk(2);
+    // u1.addRole(r1);
+
+    // const service = new UserService();
+    // const newtoken = service.createToken({email:'yadsak00@gmail.com',id:1});
+    // console.log(newtoken);
+    // const token =
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InlhZHNhazAwQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE3MDcyMTk5NTMsImV4cCI6MTcwNzIyMzU1M30.rNeSUh-B-OrcAYK2iBOV41dTyCWjqb9MuqK63CmA8qc";
+    // const response = service.verifyToken(token);
+    // console.log(response);
+  });
+};
+prepareAndStartServer();
---
+++ src/repository/user-repository.js
@@ -0,0 +1,72 @@
+const ValidationError = require("../utils/validation-error");
+const { User, Role } = require("../models/index");
+const { StatusCodes } = require("http-status-codes");
+const ClientError = require("../utils/client-error");
+
+class UserRepository {
+  async create(data) {
+    try {
+      const user = await User.create(data);
+      return user;
+    } catch (error) {
+      if (error.name == "SequelizeValidationError") {
+        throw new ValidationError(error);
+      }
+      console.log("Something went wrong on repository layer");
+      throw error;
+    }
+  }
+  async destroy(userId) {
+    try {
+      await User.destroy({ where: { id: userId } });
+      return true;
+    } catch (error) {
+      console.log("Something went wrong on repository layer");
+      throw error;
+    }
+  }
+  async getById(userId) {
+    try {
+      const user = await User.findByPk(userId, {
+        attributes: ["email", "id"],
+      });
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on repository layer");
+      throw error;
+    }
+  }
+  async getEmail(userEmail) {
+    try {
+      const user = await User.findOne({
+        where: { email: userEmail },
+      });
+      if (!user) {
+        throw new ClientError(
+          "AttributeNotFound", // name of error
+          "Invalid email sent in request", // message
+          "Please check the email, as there is no record of the email", //explanation
+          StatusCodes.NOT_FOUND
+        );
+      }
+      return user;
+    } catch (error) {
+      console.log("Something went wrong on repository layer");
+      throw error;
+    }
+  }
+  async isAdmin(userId) {
+    try {
+      const user = await User.findByPk(userId);
+      const adminRole = await Role.findOne({
+        where: { name: "ADMIN" },
+      });
+      return user.hasRole(adminRole); // 'adminRole' is sequelize object
+    } catch (error) {
+      console.log("Something went wrong on repository layer");
+      throw error;
+    }
+  }
+}
+
+module.exports = UserRepository;
